6f4ca22a3997ff5db6255b161b7821338ef74e78
Pavel Tolkach <pb.tolkach@outlook.com>
2023-02-10 17:12:43 +0300

    Add project files.

---
+++ Main.cs
@@ -0,0 +1,31 @@
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace M3Task82
+{
+    [Transaction(TransactionMode.Manual)]
+    public class Main : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            Document doc = commandData.Application.ActiveUIDocument.Document;
+
+                View3D view3D = new FilteredElementCollector(doc)
+                    .OfClass(typeof(View3D))
+                    .Cast<View3D>()
+                    .FirstOrDefault(v => v.ViewType == ViewType.ThreeD && v.Name.Equals("{3D}"));
+
+                var nwcOption = new NavisworksExportOptions();
+
+                doc.Export(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), "export.nwc", nwcOption);
+
+            return Result.Succeeded;
+        }
+    }
+}
